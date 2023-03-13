6188fc2b3fd239959852290a93f049201d0aaf66
nicole <nicolenamayan133@gmail.com>
2023-03-13 12:10:32 +0300

    add script.js,add index.html

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 const h2 = document.createElement("h2");
-h2.textContent = "I'm a software developer";
+h2.textContent = "I'm a software developer.";
 document.querySelector("body").appendChild(h2);
