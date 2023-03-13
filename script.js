b8949308e58d59a1182493b4f6eba2f3acdd0d16
nicole <nicolenamayan133@gmail.com>
2023-03-13 21:25:48 +0300

    completed assignment

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 const h2 = document.createElement("h2");
-h2.textContent = "I'm a junior software developer.";
+h2.textContent = "I'm not comfortable with settling,and am always looking for an opportunity to do better.";
 document.querySelector("body").appendChild(h2);
